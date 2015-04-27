322b7a3f21f799080419f8ed991968b3fbf6a755
Dimas Aresuki <dimasaresuki@gmail.com>
2015-04-27 12:52:40 +0700

    Create comments.js

---
+++ comments.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+   var aa=$("#da-credit").val();
+   if (aa == null) {
+       window.location.href = "http://www.dimasaresuki.com/";
+   };
+       $("#da-credit").attr("href","http://www.dimasaresuki.com/");
+});
